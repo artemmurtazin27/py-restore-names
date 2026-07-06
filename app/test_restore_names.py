6ce4188b5887560fc314bb852a9b7c7715b1f799
artemmurtazin27 <artemmurtazin27@gmail.com>
2026-07-06 16:08:29 +0300

    passed flake8

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,12 +1,18 @@
 import pytest
 from app.restore_names import restore_names
 
+
 @pytest.mark.parametrize(
     "users",
     [
-        ([{"first_name": "John", "last_name": "Doe", "full_name": "John Doe"}]),
-        ([{"first_name": None, "last_name": "Jackson", "full_name": "Bob Jackson"}]),
-        ([{"last_name": "Williams", "full_name": "Kevin Williams"}])
+        ([{"first_name": "John",
+           "last_name": "Doe",
+           "full_name": "John Doe"}]),
+        ([{"first_name": None,
+           "last_name": "Jackson",
+           "full_name": "Bob Jackson"}]),
+        ([{"last_name": "Williams",
+           "full_name": "Kevin Williams"}])
     ]
 )
 def test_restore_names(users: list) -> None:
